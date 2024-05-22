f7c405e32a4314d37e4ac7e70fbff7bc2e07882b
juruohejiarui <1466196957@qq.com>
2024-05-22 18:57:10 +0800

    update style

--- article.js
+++ article.js
@@ -14,10 +14,24 @@ window.onload = function() {
 	fetch(params.path).then(response => response.text()).then(text => {
 		contentDiv.innerHTML = text;
 	});
+
+	windowSizeChange();
 }
+
+window.onresize = windowSizeChange;
 
 function showArticleInfo(articleInfo, params) {
 	title = document.getElementById("title");
 	title.innerHTML = articleInfo.title;
 	document.title = articleInfo.title;
+}
+
+function windowSizeChange() {
+	var contentDiv = document.getElementById("content");
+	const windowSize = { width: window.innerWidth, height: window.innerHeight };
+	if (windowSize.height > windowSize.width)
+		contentDiv.style.margin = "0px";
+	else {
+		contentDiv.style.margin = "0px 10% 0px 10%";
+	}
 }
